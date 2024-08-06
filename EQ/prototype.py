0db2e54be09008931d1743e66e102c8b4f13eac0
ayh2 <ayh2@andrew.cmu.edu>
2024-08-06 15:16:43 -0400

    updated with RLS adaptive filtering

--- EQ/prototype.py
+++ EQ/prototype.py
@@ -3,84 +3,48 @@ import scipy
 from scipy import signal
 from scipy.signal import tf2zpk
 import matplotlib.pyplot as plt
+import argparse
 
-# class EQFilter:
-#     def __init__(self, N, type="lowpass"):
-N = 4
-ks = np.arange(1, N+1)
-gamma_ks = np.pi*((2*ks-1)/N-1)/4
-c = 1
-for i in range(len(ks)):
-    c /= (2*np.cos(gamma_ks[i]))
-numerators_roots = (-1, )*N
-numerators = c*np.poly(numerators_roots)
-print(numerators)
-denominators_roots = np.array([]) if N == 1 else 1j*np.tan((2*ks-N-1)*np.pi/4/N)
-denominators = np.array([1]) if N == 1 else np.poly(denominators_roots) 
-print(denominators_roots)
-w, h = signal.freqz(numerators, denominators)
-# print(tf2zpk(numerators, denominators))
-fig, ax1 = plt.subplots()
-ax1.set_title('Digital filter frequency response')
-ax1.plot(w/np.pi, abs(h)**2, 'b')
-ax1.set_ylabel('Amplitude [dB]', color='b')
-ax1.set_xlabel('Frequency [rad/sample]')
-plt.show()
+class CanonicalLPF:
+    def __init__(self, order):
+        self.N = order
+        ks = np.arange(1, self.N+1)
+        gamma_ks = np.pi*((2*ks-1)/self.N-1)/4
+        self.canon_g = 1
+        for i in range(len(ks)):
+            self.canon_g /= (2*np.cos(gamma_ks[i]))
+        self.canon_numerators_roots = (-1, )*self.N
+        numerators = self.canon_g*np.poly(self.canon_numerators_roots)
+        self.canon_denominators_roots = 1j*np.tan((2*ks-self.N-1)*np.pi/4/self.N)
+        denominators = np.poly(self.canon_denominators_roots)
 
-# import numpy as np
-# import scipy.signal as signal
-
-# # Define the order of the filter
-# N = 4  # Example order
-
-# # Calculate the gamma values
-# k = np.arange(1, N + 1)
-# gamma_k = np.pi * ((2 * k - 1) / (N - 1)) / 4
-
-# # Calculate the coefficients of the numerator and denominator
-# numerator_coeffs = [1] + [1]
-# denominator_coeffs = []
-
-# for gamma in gamma_k:
-#     a1 = 2 * np.cos(gamma)
-#     a0 = -1j * np.tan(gamma)
-#     denominator_coeffs.append([a1, a0, 1])
-
-# # Combine the sections
-# num_combined = numerator_coeffs
-# den_combined = denominator_coeffs[0]
-
-# for i in range(1, N):
-#     den_combined = np.convolve(den_combined, denominator_coeffs[i])
-
-# # Normalize the coefficients
-# den_combined = np.real(den_combined / den_combined[0])
-
-# # Print the time-domain coefficients
-# print("Numerator coefficients:", num_combined)
-# print("Denominator coefficients:", den_combined)
-# import matplotlib.pyplot as plt
-
-# # Frequency response
-# w, h = signal.freqz(num_combined, den_combined)
-
-# # Plot frequency response
-# plt.plot(w, 20 * np.log10(abs(h)))
-# plt.title('Frequency response')
-# plt.xlabel('Frequency [radians / second]')
-# plt.ylabel('Amplitude [dB]')
-# plt.grid()
-# plt.show()
-
-# # Impulse response
-# impulse = np.zeros(100)
-# impulse[0] = 1
-# response = signal.lfilter(num_combined, den_combined, impulse)
-
-# # Plot impulse response
-# plt.stem(response)
-# plt.title('Impulse response')
-# plt.xlabel('Samples')
-# plt.ylabel('Amplitude')
-# plt.grid()
-# plt.show()
+    def set_cutoff(self, fc):
+        alpha = (np.tan(fc/2)-1)/(np.tan(fc/2)+1)
+        beta = (1-np.tan(fc/2))/(1+np.tan(fc/2))
+        # beta = np.sin((np.pi/2-fc)/2)/np.sin((np.pi/2+fc)/2)
+        denominators_roots = np.array([(p+beta)/(1+beta*p) for p in self.canon_denominators_roots])
+        print(self.canon_denominators_roots)
+        print(denominators_roots)
+        denominators = np.poly(denominators_roots)
+        print(denominators)
+        g = self.canon_g 
+        for _ in range(self.N):
+            g *= (1-beta)
+        numerators = g*np.poly(self.canon_numerators_roots)
+        
+        w, h = signal.freqz(numerators, denominators)
+        fig, ax1 = plt.subplots()
+        ax1.set_title('Digital filter frequency response')
+        ax1.plot(w/np.pi, abs(h)**2, 'b')
+        ax1.set_ylabel('Amplitude [dB]', color='b')
+        ax1.set_xlabel('Frequency [rad/sample]')
+        plt.show()
+        
+        
+        
+if __name__ == '__main__':
+    lpf = CanonicalLPF(2)
+    lpf.set_cutoff(np.pi/4)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-t', '--type', type=str, default='lowpass', help='Filter type: lowpass, bandpass, highpass, shelf')
+    # parser.add_argument('-o', '--order', type=int, default='1', help='Enter a filter order between 1 and 8 inclusive')
--- adaptive_filtering/adaptive.py
+++ adaptive_filtering/adaptive.py
@@ -5,31 +5,57 @@ class AF_LMS:
     def __init__(self, step_size, filt_ord, primary, reference):
         self.N = min(len(primary), len(reference))
         self.W = np.zeros(filt_ord)
-        self.u = step_size
+        self.W[(filt_ord-1)//2] = 1
         self.pri = primary[:self.N]
         self.ref = reference[:self.N]
         self.L = filt_ord 
+        self.u = step_size/self.L/np.var(self.ref)
         self.out = np.zeros(self.N)
-        self.Xs = np.zeros((self.N, self.L))
     def process(self):
         for k in range(self.N):
-            Xk = np.append(self.ref[:k], np.array([0]*(self.L-k))) if k < self.L else self.ref[k-self.L:k]
+            Xk = np.append(np.array([0]*(self.L-k-1)), self.ref[:k+1]) if k < self.L else self.ref[k-self.L+1:k+1]
             Xk = np.flip(Xk)
-            self.Xs[k, :] = Xk
             yk = np.dot(Xk, self.W)
-            dk = self.pri[k]
-            self.W -= 2*self.u*Xk*(yk-dk)
-            self.out[k] = dk-yk 
+            eps = self.pri[k]-yk 
+            gradient = -2*eps*Xk
+            self.W -= self.u*gradient
+            self.out[k] = eps
         return self.out
-        # for k in range(self.N):
-        #     self.out[k] = np.dot(self.Xs[k, :], self.W)
-        # return self.pri-self.out
-            
+
+# Recursive Least Square, aka LMS-Newton
+class AF_RLS:
+    def __init__(self, step_size, filt_ord, primary, reference):
+        self.N = min(len(primary), len(reference))
+        self.W = np.zeros(filt_ord) 
+        self.W[(filt_ord-1)//2] = 1
+        self.u = step_size 
+        self.pri = primary[:self.N]
+        self.ref = reference[:self.N] 
+        self.L = filt_ord
+        self.out = np.zeros(self.N)
+        self.R_inv_hat = np.identity(self.L)/np.var(self.ref)
+    def process(self, alpha = 1e-4):
+        for k in range(self.N):
+            Xk = np.append(np.array([0]*(self.L-k-1)), self.ref[:k+1]) if k < self.L else self.ref[k-self.L+1:k+1]
+            Xk = np.flip(Xk)
+            Sk_hat = self.R_inv_hat@Xk
+            Sk_hat = Sk_hat.reshape(-1, 1)
+            self.R_inv_hat = (self.R_inv_hat-alpha*(Sk_hat@Sk_hat.T)/(1-alpha+alpha*np.dot(Xk, Sk_hat)))/(1-alpha)
+            dk = self.pri[k]
+            ek = dk-np.dot(Xk, self.W)
+            self.W += 2*self.u*ek*(self.R_inv_hat@Xk)
+            self.out[k] = ek
+        return self.out
+        
 if __name__ == '__main__':
     pri, sr = sf.read("primary.wav")
     ref, sr = sf.read("reference.wav")
-    lms = AF_LMS(0.02, 20, pri, ref) 
-    output = lms.process()
-    sf.write("output_lms.wav", output, sr)
+    lms = AF_LMS(0.06, 60, pri, ref)
+    output_lms = lms.process()
+    sf.write("output_lms.wav", output_lms, sr)
+    rls = AF_RLS(1e-4, 60, pri, ref)
+    output_rls = rls.process()
+    sf.write("output_rls.wav", output_rls, sr)
+    
     
         
